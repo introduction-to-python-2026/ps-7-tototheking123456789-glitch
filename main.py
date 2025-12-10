f1fa58636c02de4bc8c21bbd3816c946afb590ea
YotamHibner <tototheking123456789@gmail.com>
2025-12-10 15:12:57 +0200

    Visualize selected features with histograms and scatter plots
    Added histograms and scatter plots for selected features.

--- main.py
+++ main.py
@@ -19,3 +19,22 @@ features = list(df.columns)
 print("Available features:", features)
 selected_features = ['MedInc', 'HouseAge', 'AveRooms', 'AveBedrms', 'Population']
 print("Selected features: ", selected_features)
+
+
+fig, axs  = plt.subplots(1, len(selected_features), figsize = (20,3))
+
+for ax, f in zip(axs, selected_features):
+    ax.hist(df[f], bins=5, color='skyblue', edgecolor='black')
+    ax.set_xlabel(f)
+
+
+reference_feature = selected_features[0]  
+y = df[reference_feature]
+
+fig, axs  = plt.subplots(1, len(selected_features), figsize = (20,3))
+
+for ax, f in zip(axs, selected_features):
+  ax.scatter(df[f], y)
+  ax.set_xlabel(f)
+
+plt.show()
