24144c3f04bfaef7a0071cde0186ef71267a3c55
YotamHibner <tototheking123456789@gmail.com>
2025-12-10 15:06:07 +0200

    Print dataset description for california_housing

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ import seaborn as sns
 from sklearn.datasets import fetch_openml
 import pandas as pd
 data = fetch_openml(name='california_housing', version=1, as_frame=True,parser='pandas')
+print(data.DESCR)
